045aaa42c64240c2e2d78b11f36db6094a511e31
EcemAydn <ecemaydn15@gmail.com>
2023-03-24 18:32:18 +0300

    create navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div
+    class="flex bg-zinc-600 items-center justify-between p-4 md:p-6 font-light text-white rounded-b-md"
+  >
+    <h1>NEWS</h1>
+    <div class="flex gap-8">
+      <RouterLink
+        to="/"
+        active-class="border-red-300 text-red-300"
+        class="border-b-2 p-2"
+        >Home
+      </RouterLink>
+      <RouterLink
+        to="/sports"
+        active-class="border-red-300 text-red-300"
+        class="border-b-2 p-2"
+        >Sport
+      </RouterLink>
+      <RouterLink
+        to="/business"
+        active-class="border-red-300 text-red-300"
+        class="border-b-2 p-2"
+        >Business
+      </RouterLink>
+    </div>
+  </div>
+</template>
