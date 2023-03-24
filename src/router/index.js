647ef3200f4cd08f75567c07993d096f18f2e175
EcemAydn <ecemaydn15@gmail.com>
2023-03-24 18:34:45 +0300

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,17 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/sports",
+      name: "sports",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/SportsView.vue"),
+    },
+    {
+      path: "/business",
+      name: "business",
+      component: () => import("../views/BusinessView.vue"),
     },
   ],
 });
