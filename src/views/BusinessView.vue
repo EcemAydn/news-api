7c911ca411452b2f536d63f13fae1babd12dd7df
EcemAydn <ecemaydn15@gmail.com>
2023-03-27 08:38:33 +0300

    add business list

--- src/views/BusinessView.vue
+++ src/views/BusinessView.vue
@@ -6,9 +6,39 @@ const newsStore = useNewsStore();
 
 newsStore.getApiBusiness();
 </script>
-
 <template>
-  <div class="w-full h-full">
+  <div class="w-full h-full relative">
     <Navbar />
+    <div
+      v-for="newitem in newsStore.newsBusiness"
+      :key="newitem.id"
+      class="w-full h-full flex gap-6 relative"
+    >
+      <img
+        :src="newitem.urlToImage"
+        alt="Photo"
+        class="border-8 absolute rounded-md top-8 flex blur-sm"
+      />
+    </div>
+    <div
+      class="w-full h-full grid md:grid-cols-3 gap-6 md:gap-10 p-8 relative bottom-0 top-96"
+    >
+      <div
+        v-for="newitem in newsStore.newsBusiness"
+        :key="newitem.id"
+        class="flex flex-col w-full h-full gap-6 bg-white shadow-md p-4 rounded-md"
+      >
+        <div class="font-bold">
+          {{ newitem.title }}
+        </div>
+        <div>
+          <img :src="newitem.urlToImage" alt="Photo" class="rounded-md" />
+        </div>
+        <div>
+          {{ newitem.description }}
+          <a :href="newitem.url" class="font-bold">continue</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
