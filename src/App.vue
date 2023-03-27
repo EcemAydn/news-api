e021a49a0df2639cf620030c71507bb135132c31
EcemAydn <ecemaydn15@gmail.com>
2023-03-27 08:34:44 +0300

    remove height

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="w-full h-full bg-zinc-100">
+  <div class="w-full bg-zinc-100">
     <RouterView />
   </div>
 </template>
