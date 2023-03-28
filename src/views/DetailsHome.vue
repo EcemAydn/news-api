1b0088e53ae87cad8c718556ca430c976caa263b
EcemAydn <ecemaydn15@gmail.com>
2023-03-28 19:05:13 +0300

    create detail page

--- src/views/DetailsHome.vue
+++ src/views/DetailsHome.vue
@@ -1,40 +1,45 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
 import { useNewsStore } from "../stores/NewsStore";
+import { useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
 
 const newsStore = useNewsStore();
+const route = useRoute();
+const newList = ref({
+  title: "",
+  img: "",
+  description: "",
+});
+
+onMounted(async () => {
+  await newsStore.getApi();
+  if (route.params.id) {
+    newList.value = newsStore.getNewsById(route.params.id);
+    console.log(newList.value);
+    console.log(route.params.id);
+  } else {
+    console.log("olmadı");
+  }
+});
 
 newsStore.getApi();
 </script>
 <template>
-  <div class="w-full h-full">
-    <Navbar />
-    <div class="w-full h-full grid md:grid-cols-3 gap-6 md:gap-10 p-8">
-      <div
-        v-for="newitem in newsStore.news"
-        :key="newitem.id"
-        class="flex flex-col w-full h-full gap-6 bg-white shadow-md p-4 rounded-md"
-      >
-        <div class="font-bold">
-          {{ newitem.title }}
+  <Navbar countryShow="false" />
+  <div class="w-full flex flex-col items-center justify-center p-8 bg-zinc-100">
+    <div
+      class="w-fit h-fit flex items-center p-8 justify-center bg-white rounded-md shadow-lg"
+    >
+      <div class="flex flex-col gap-8 relative">
+        <img :src="newList.urlToImage" alt="Photo" />
+        <div
+          class="absolute z-20 text-white bottom-40 text-2xl bg-gradient-to-r from-black font-bold p-8"
+        >
+          {{ newList.title }}
         </div>
-        <div>
-          <img
-            :src="newitem.urlToImage === null ? randomUrl : newitem.urlToImage"
-            alt="Photo"
-            class="rounded-md h-[300px]"
-          />
-        </div>
-        <div>
-          {{
-            newitem.description
-              ? newitem.description
-              : newitem.content
-              ? newitem.content
-              : "-"
-          }}
-          <a :href="newitem.url" class="font-bold text-blue-400">continue</a>
-        </div>
+        <div>{{ newList.description }}</div>
+        <a :href="newList.url" class="font-bold">Click for more news</a>
       </div>
     </div>
   </div>
