73183ed46571db17f7e901529b76d8587ab6ce79
EcemAydn <ecemaydn15@gmail.com>
2023-03-28 21:40:11 +0300

    Create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-black p-16 grid grid-cols-3 gap-8 text-white font-thin">
+    <div class="col-span-2">
+      <h1 class="bg-gradient-to-r from-white text-black font-black">News</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, vero
+        facilis fuga voluptatibus quasi libero suscipit voluptatum explicabo non
+        quo debitis, voluptates quae quos amet sapiente delectus porro.
+      </p>
+      <p class="pt-4">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, at?
+      </p>
+    </div>
+    <div class="grid grid-cols-3 gap-2 items-center justify-center">
+      <a href="#" class="text-blue-500 border-b w-fit">Twitter</a>
+      <a href="#" class="text-blue-600 border-b w-fit">Facebook</a>
+      <a href="#" class="text-pink-800 border-b w-fit">Instagram</a>
+      <a href="#" class="text-white border-b w-fit">Phone</a>
+      <a href="#" class="text-blue-400 border-b w-fit">Mail</a>
+      <a href="#" class="text-yellow-400 border-b w-fit">About</a>
+    </div>
+  </div>
+</template>
