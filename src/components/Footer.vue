56950df095965c3117002e217359a1b3068fc0ac
EcemAydn <ecemaydn15@gmail.com>
2023-03-29 14:22:53 +0300

    add icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,18 @@
+<script setup>
+import { Twitter } from "lucide-vue-next";
+import { Facebook } from "lucide-vue-next";
+import { Instagram } from "lucide-vue-next";
+import { Phone } from "lucide-vue-next";
+import { Mail } from "lucide-vue-next";
+</script>
 <template>
-  <div class="bg-black p-16 grid grid-cols-3 gap-8 text-white font-thin">
-    <div class="col-span-2">
-      <h1 class="bg-gradient-to-r from-white text-black font-black">News</h1>
+  <div class="bg-white p-8 grid md:grid-cols-3 gap-8 text-black font-light">
+    <div class="md:col-span-2 text-xs md:text-base">
+      <h1
+        class="bg-gradient-to-r from-white text-base md:text-lg text-black font-black"
+      >
+        News
+      </h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, vero
         facilis fuga voluptatibus quasi libero suscipit voluptatum explicabo non
@@ -11,13 +22,17 @@
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, at?
       </p>
     </div>
-    <div class="grid grid-cols-3 gap-2 items-center justify-center">
-      <a href="#" class="text-blue-500 border-b w-fit">Twitter</a>
-      <a href="#" class="text-blue-600 border-b w-fit">Facebook</a>
-      <a href="#" class="text-pink-800 border-b w-fit">Instagram</a>
-      <a href="#" class="text-white border-b w-fit">Phone</a>
-      <a href="#" class="text-blue-400 border-b w-fit">Mail</a>
-      <a href="#" class="text-yellow-400 border-b w-fit">About</a>
+    <div class="flex gap-6 items-center justify-center">
+      <a href="#" class="text-blue-500"><Twitter /></a>
+      <a href="#" class="text-blue-600"><Facebook /></a>
+      <a href="#" class="text-pink-500"><Instagram /></a>
+      <a href="#" class="text-green-500"><Phone /></a>
+      <a href="#" class="text-blue-400"><Mail /></a>
     </div>
+  </div>
+  <div
+    class="w-full flex items-center text-xs md:text-base justify-center bg-gray-100 text-black font-thin"
+  >
+    ecembeyzaaydin
   </div>
 </template>
